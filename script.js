826a1b5b7022422b64f85047e06da4da9f7f0417
Lilieth Betancourt <liliethordonez@yahoo.com>
2023-03-25 22:11:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    image: "",
+    title: "",
+    description: "",
+    link: "",
+    linkText: '',
+  }, 
+]
+let text = '';
+products.forEach(function(product, index) {
+	text = text +
+  
+  ``;
+});
+
+document.querySelector('.swiper-wrapper').innerHTML = text;
